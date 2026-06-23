b420539632a9f36debd006df43055eb945baf1b4
Giedrius Gudzinskas <giedrius.gudzinskas@gmail.com>
2026-06-23 21:06:25 +0300

    Wire Stripe test payment links for all 17 SKUs

--- main.js
+++ main.js
@@ -18,9 +18,23 @@
      While these are empty, Checkout shows setup instructions.
   --------------------------------------------------------------- */
   var PAYMENT_LINKS = {
-    // "low-back-pain__short": "",
-    // "low-back-pain__long": "",
-    // "all-access": ""
+    "low-back-pain__short":       "https://buy.stripe.com/test_cNi3co3rYcTL0Pm0Eie7m00",
+    "low-back-pain__long":        "https://buy.stripe.com/test_14AaEQ3rY7zr55CaeSe7m01",
+    "neck-pain__short":           "https://buy.stripe.com/test_6oU3co5A6g5X0Pm4Uye7m02",
+    "neck-pain__long":            "https://buy.stripe.com/test_5kQbIU9Qm8Dv8hO9aOe7m03",
+    "shoulder__short":            "https://buy.stripe.com/test_eVqcMY6Ea2f79lScn0e7m04",
+    "shoulder__long":             "https://buy.stripe.com/test_5kQcMY8Mi2f77dK4Uye7m05",
+    "knee-pain__short":           "https://buy.stripe.com/test_dRm9AM3rY4nfgOkdr4e7m06",
+    "knee-pain__long":            "https://buy.stripe.com/test_7sY7sE3rY1b341y2Mqe7m07",
+    "plantar-fasciitis__short":   "https://buy.stripe.com/test_4gM00ce6Cf1T1Tq1Ime7m08",
+    "plantar-fasciitis__long":    "https://buy.stripe.com/test_28EfZae6C9Hz2XuaeSe7m09",
+    "tennis-elbow__short":        "https://buy.stripe.com/test_7sY28kaUq6vn1Tq2Mqe7m0a",
+    "tennis-elbow__long":         "https://buy.stripe.com/test_7sY28kbYubPH1Tq3Que7m0b",
+    "hip-glute__short":           "https://buy.stripe.com/test_4gMcMYfaG6vn1Tq1Ime7m0c",
+    "hip-glute__long":            "https://buy.stripe.com/test_aFa28k5A63jbdC8fzce7m0d",
+    "ankle-sprain__short":        "https://buy.stripe.com/test_cNifZa1jQaLD8hOfzce7m0e",
+    "ankle-sprain__long":         "https://buy.stripe.com/test_aFa3co4w23jbbu086Ke7m0f",
+    "all-access":                 "https://buy.stripe.com/test_eVq28kfaG1b3dC81Ime7m0g"
   };
 
   // ---- helpers ----
